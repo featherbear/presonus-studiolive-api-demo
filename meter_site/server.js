0c579872cebc90c9775ae2995a0c17f35a0ea977
Andrew Wong <andrew.j.wong@outlook.com>
2020-10-23 18:17:08 +1100

    Add package dependencies and dotenv

--- meter_site/server.js
+++ meter_site/server.js
@@ -1,77 +1,78 @@
-"use strict";
+'use strict'
 
-const CONSOLE_HOST = "10.185.192.6";
-const CONSOLE_PORT = 53000;
+require('dotenv').config()
 
-const SERVER_HOST = "0.0.0.0";
-const SERVER_PORT = 8080;
+const fastify = require('fastify')()
+const socketio = require('socket.io')(fastify.server).of('/meter')
+const api = require('presonus-studiolive-api')
+const path = require('path')
 
-let emulateConsole = true;
+const {
+  CONSOLE_HOST,
+  CONSOLE_PORT,
+  SERVER_HOST,
+  SERVER_PORT,
+  EMULATE_CONSOLE
+} = process.env
 
-//
-
-const fastify = require("fastify")();
-const socketio = require("socket.io")(fastify.server).of("/meter");
-const path = require("path");
-
-let PreSonusAPI;
-if (emulateConsole) {
+let PreSonusAPI
+if (EMULATE_CONSOLE) {
   PreSonusAPI = {
     Client: class DummyClient {
       /* Dummy client to emulate events */
-      constructor() {
-        this.i = 0; // Emulate rise and fall of values
+      constructor () {
+        this.i = 0 // Emulate rise and fall of values
         setInterval(() => {
           // Emit a metering event every 50ms
-          socketio.emit("meter", this.metering);
-        }, 50);
+          socketio.emit('meter', this.metering)
+        }, 50)
       }
-      listen() {
+      listen () {
         /* Stub */
       }
-      connect() {
+      connect () {
         /* Stub */
       }
-      on() {
+      on () {
         /* Stub */
       }
-      get metering() {
-        let data = [];
+      get metering () {
+        let data = []
         for (var i = 0; i < 32; i++) {
           data.push(
             Math.abs(parseInt(Math.random() * 65535 * Math.sin(this.i / 50)))
-          );
+          )
         }
-        this.i++;
+        this.i++
         return {
           level: data
-        };
+        }
       }
     }
-  };
+  }
 } else {
-  PreSonusAPI = require("../PreSonusAPI");
+  PreSonusAPI = require('../PreSonusAPI')
 }
 
-console.info("Starting server...");
-let client = new PreSonusAPI.Client(CONSOLE_HOST, CONSOLE_PORT);
+console.info('Starting server...')
+let client = new PreSonusAPI.Client(CONSOLE_HOST, CONSOLE_PORT)
 
-console.debug(`Listening to UDP messages`);
-client.listen();
-client.on("meter", data => socketio.emit("meter", data));
+console.debug(`Listening to UDP messages`)
+client.listen()
+client.on('meter', data => socketio.emit('meter', data))
 
-console.debug(`Connecting to console on ${CONSOLE_HOST}:${CONSOLE_PORT}`);
-client.connect();
+console.debug(`Connecting to console on ${CONSOLE_HOST}:${CONSOLE_PORT}`)
+client.connect()
 
-fastify.register(require("fastify-static"), {
-  root: path.join(__dirname, "public")
-});
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'public')
+})
 
 fastify.listen(SERVER_PORT, SERVER_HOST, err => {
   if (err) {
-    fastify.log.error(err);
-    process.exit(1);
+    fastify.log.error(err)
+    process.exit(1)
   }
-  let address = fastify.server.address();
-  console.info(`Server listening on ${address.address}:${address.port}`);
-});
+  let address = fastify.server.address()
+  console.info(`Server listening on ${address.address}:${address.port}`)
+})
