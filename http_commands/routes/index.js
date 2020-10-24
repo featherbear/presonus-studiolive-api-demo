b14791b55e666dcd4e4b4a1d248f93206e2aaf3e
Andrew Wong <andrew.j.wong@outlook.com>
2020-10-24 14:25:48 +1100

    Add HTTP server
    Supports muting and unmuting of line channels

---
+++ http_commands/routes/index.js
@@ -0,0 +1,37 @@
+module.exports = require('fastify-plugin')(function (app, opts, next) {
+  app.get(
+    '/mute/ch/:channel',
+    {
+      schema: {
+        description: 'Mute a line channel',
+        params: {
+          channel: { type: 'integer', description: 'Channel ID' }
+        }
+      }
+    },
+    (req, res) => {
+      const { channel } = req.params
+      app.slApi.mute(channel)
+      res.send('OK')
+    }
+  )
+
+  app.get(
+    '/unmute/ch/:channel',
+    {
+      schema: {
+        description: 'Unmute a line channel',
+        params: {
+          channel: { type: 'integer', description: 'Channel ID' }
+        }
+      }
+    },
+    (req, res) => {
+      const { channel } = req.params
+      app.slApi.unmute(channel)
+      res.send('OK')
+    }
+  )
+
+  next()
+})
---
+++ http_commands/server.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+
+const { Client } = require('presonus-studiolive-api')
+const fastify = require('fastify')()
+const { CONSOLE_HOST, CONSOLE_PORT, SERVER_HOST, SERVER_PORT } = process.env
+
+if (!CONSOLE_HOST || !CONSOLE_PORT || !SERVER_HOST || !SERVER_PORT) {
+  console.log('Misconfigured server.')
+  process.exit(1)
+}
+
+fastify.decorate(
+  'slApi',
+  (function () {
+    let api = new Client(CONSOLE_HOST, CONSOLE_PORT)
+    api.connect()
+    return api
+  })()
+)
+
+fastify.register(require('./routes'))
+
+fastify.listen(SERVER_PORT, SERVER_HOST, err => {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  let address = fastify.server.address()
+  console.info(`Server listening on ${address.address}:${address.port}`)
+})
